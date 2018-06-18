49bdc918c0572d9f42994759a628bdf2d8591a7b
Shreya <shreya.g.gupta@impetus.co.in>
2018-06-18 18:33:32 +0530

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   DemoMVC/.classpath
    	new file:   DemoMVC/.project
    	new file:   DemoMVC/.settings/.jsdtscope
    	new file:   DemoMVC/.settings/org.eclipse.jdt.core.prefs
    	new file:   DemoMVC/.settings/org.eclipse.m2e.core.prefs
    	new file:   DemoMVC/.settings/org.eclipse.wst.common.component
    	new file:   DemoMVC/.settings/org.eclipse.wst.common.project.facet.core.xml
    	new file:   DemoMVC/.settings/org.eclipse.wst.jsdt.ui.superType.container
    	new file:   DemoMVC/.settings/org.eclipse.wst.jsdt.ui.superType.name
    	new file:   DemoMVC/.settings/org.eclipse.wst.validation.prefs
    	new file:   DemoMVC/pom.xml
    	new file:   DemoMVC/src/main/java/com/impetus/AddController.java
    	new file:   DemoMVC/src/main/resources/impetus/AddController.java
    	new file:   DemoMVC/src/main/webapp/WEB-INF/impetus-servlet.xml
    	new file:   DemoMVC/src/main/webapp/WEB-INF/web.xml
    	new file:   DemoMVC/src/main/webapp/display.jsp
    	new file:   DemoMVC/src/main/webapp/index.jsp
    	new file:   DemoMVC/target/classes/com/impetus/AddController.class
    	new file:   DemoMVC/target/classes/impetus/AddController.java
    	new file:   DemoMVC/target/m2e-wtp/web-resources/META-INF/MANIFEST.MF
    	new file:   DemoMVC/target/m2e-wtp/web-resources/META-INF/maven/com.impetus/DemoMVC/pom.properties
    	new file:   DemoMVC/target/m2e-wtp/web-resources/META-INF/maven/com.impetus/DemoMVC/pom.xml
    	new file:   DemoMVC/target/m2e-wtp/web-resources/META-INF/maven/com.impetus/DemoMVC1/pom.properties
    	new file:   DemoMVC/target/m2e-wtp/web-resources/META-INF/maven/com.impetus/DemoMVC1/pom.xml

---
+++ DemoMVC/target/classes/impetus/AddController.java
@@ -0,0 +1,14 @@
+package impetus;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class AddController {
+@Controller
+	public void add()
+	{
+	@RequestMapping("/add")
+	System.out.print("Hi");
+	}
+
+}
